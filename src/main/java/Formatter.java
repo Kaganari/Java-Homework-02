d123c9a25761eba015b06b4aa8010ef2668b8feb
Kaganari <razgal94@gmail.com>
2017-10-30 15:34:28 +0600

    Output
    Fixed output, now it's into file codeOutput.txt instead of Sout.

--- src/main/java/Formatter.java
+++ src/main/java/Formatter.java
@@ -1,4 +1,4 @@
-import java.io.File;
+import java.io.FileWriter;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,15 +11,23 @@ import java.util.List;
 public class Formatter {
     public static void main(final String[] args) {
         //String sprt = File.separator;
-        //Path testFilePath = Paths.get("src"+ sprt+"main"+sprt+"resources"+sprt+"code.txt");
-        Path codeFilePath = Paths.get("src/main/resources/code.txt");//Apparently, separator "/" is working for Windows as well. Strange..
+        //Path codeFilePath = Paths.get("src"+ sprt+"main"+sprt+"resources"+sprt+"code.txt");
+        Path codeFilePath = Paths.get("src/main/resources/code.txt");  //Apparently, separator "/" is working for Windows as well. Strange..
+        //Path outputFilePath = Paths.get("src/main/resources/codeOutput.txt");
+        List<String> lines;
         try{
-            List<String> lines = Files.readAllLines(codeFilePath, Charset.defaultCharset());
+            lines = Files.readAllLines(codeFilePath, Charset.defaultCharset());
             lines = LineBreaker.LineBreaker((lines));
             lines = Tabulation.Tabulation(lines);
             for (String line : lines) {
                 System.out.println(line);
             }
+            FileWriter writer = new FileWriter("src/main/resources/codeOutput.txt", false);
+            for (int i=0;i<lines.size();i++) {
+                writer.write(lines.get(i)+System.lineSeparator());
+                writer.flush();
+            }
+            writer.flush();
         } catch (Exception e) {
             e.printStackTrace();
         }
